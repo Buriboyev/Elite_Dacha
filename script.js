a31619a1e97c5795c785ba712836b28bad5e5a14
Buriboyev <buriboyevbotir@gmail.com>
2026-03-06 13:26:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-// THEME SWITCH
-const images = document.querySelectorAll(".gallery img");
 const toggle = document.getElementById("themeToggle");
+let rotation = 0;
+// SAQLANGAN THEME
+const savedTheme = localStorage.getItem("theme");
 
-// SAQLANGAN THEME NI YUKLASH
-if (localStorage.getItem("theme") === "dark") {
+if (savedTheme === "dark") {
   document.body.classList.add("dark");
+  toggle.textContent = "☀️";
+} else {
+  toggle.textContent = "🌙";
 }
 
-// BUTTON BOSILGANDA
-if (toggle) {
-  toggle.onclick = () => {
-    document.body.classList.toggle("dark");
+toggle.addEventListener("click", () => {
 
-    if (document.body.classList.contains("dark")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-  };
+  // 360 gradus qo'shib boradi
+  rotation += 360;
+  toggle.style.transform = `rotate(${rotation}deg)`;
+
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme","dark");
+    toggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme","light");
+    toggle.textContent = "🌙";
+  }
+
+});
+const scrollTop = document.getElementById("scrollTop");
+
+if(scrollTop){
+  scrollTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// scroll bo'lganda tugma chiqadi
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 200){
+scrollBtn.classList.add("show");
+}else{
+scrollBtn.classList.remove("show");
 }
 
-// BOOKING FORM
+});
+
+// tugma bosilganda tepaga chiqadi
+scrollBtn.addEventListener("click", () => {
+
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+
+});
